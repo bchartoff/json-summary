bd8c5a11be75b0a89f4b00544e9a7ff2fac47ecb
Ben Chartoff <bchartoff@gmail.com>
2014-03-04 17:05:41 -0500

    read json as ordered dict. Began error handling.

--- jsonread.py
+++ jsonread.py
@@ -1,35 +1,85 @@
+import collections
+import os
+import sys
+import binascii
+from pprint import pprint
+import json
+
+
+jsonIsList = False
+tempListKey = ""
+listThreshold = 5
+
+def setTempListKey():
+  return "json-summary-temporary-key%s"%binascii.b2a_hex(os.urandom(15))
+
+def openJson(fileName):
+  global jsonIsList
+  global tempListKey
+  with open(fileName) as inFile:
+    while True:
+      char = inFile.read(1)
+      if not char:
+        raise Exception("Malformed JSON, does not begin with { or [")
+        break
+      if char == "[":
+        jsonIsList = True
+        tempListKey = setTempListKey()
+        inFile.seek(0,0)
+        tempFileString = "{\"%s\":%s}"%(tempListKey,inFile.read())
+        jsonData = json.loads(tempFileString, object_pairs_hook=collections.OrderedDict)
+        break
+      if char == "{":
+        inFile.seek(0,0)
+        jsonData = json.load(inFile, object_pairs_hook=collections.OrderedDict)
+        break
+  return jsonData
+      #elif char == "{"
+# def summarize(inJson):
+
+
+# def summarizeList(inList):
+#   for item in inList:
+    
+
+
 #out = {}
 #A
 #func of dict, 
 #for key,val in dict
-	#if val is nothing
-		#dict[key] = val
-	#if val is dict
-		#func(val)
-	#if val is list
-		#if val[0] is nothing
-			#dict[key] = val[0]
-		#if val[0] is dict
-			#dict[key] = func(val[0])
-listThreshold = 5
+  #if val is nothing
+    #dict[key] = val
+  #if val is dict
+    #func(val)
+  #if val is list
+    #if val[0] is nothing
+      #dict[key] = val[0]
+    #if val[0] is dict
+      #dict[key] = func(val[0])
 
-def f1(inDict):
-	for key in inDict:
-		val = inDict[key]
-		if isinstance(val, dict):
-			f1(val)
-		elif isinstance(val,list):
-			if isinstance(val[0], dict):
-				inDict[key] = f1(val[0])
-			else:
-				inDict[key] = [val[0]]
-		else:
-			inDict[key] = val
-	return inDict
+def comment(commentType,num=0):
+  if commentType == "endList":
+    return "******* List contains %i total entries *******"%num
+
+def summarizeList(inList):
+
+def summarizeDict(inDict):
+  for key in inDict:
+    val = inDict[key]
+    if isinstance(val, dict):
+      summarizeDict(val)
+    elif isinstance(val,list):
+      if isinstance(val[0], dict):
+        inDict[key] = summarizeDict(val[0])
+      else:
+        #inDict[key] = [val[0]]
+        inDict[key] = val
+    else:
+      inDict[key] = val
+  return inDict
 
 
 
-import json
 
 
 # def get_all(data):
@@ -47,65 +97,52 @@ import json
 
 
 
-jsonFileLarge = open('sf-city-lots-json/citylots.json')
-jsonFileSample = open('jsonsample.json')
-jsonFileComplex = open('cakejson.json')
+#jsonFileLarge = open('sf-city-lots-json/citylots.json')
+jsonFileSample = 'jsonsample.json'
+jsonFileComplex = 'cakejson.json'
 
 jsonFile = jsonFileSample
-jsonData = json.load(jsonFile)
 
-test = {
-	"top":
-		{
-			"cat1":
-				[
-					{
-						"cat1A": "val",
-						"cat1B": "val",
-						"cat1C":
-							{
-								"cat1CA":
-									[
-										{ "id": [1,2,3], "type": "Regular" },
-										{ "id": [1,2,3], "type": "Regular" }
-									]
-							},
-						"cat1D":
-							[
-								{ "id": "5001", "type": 
-													{
-														"bottom":
-															[
-																{"bottomest":"val"},
-																{"bottomest2":"val"}
-															]
-													}
-								}
-							]
-					}
-				]
-		}
-}
-print f1(jsonData)
+#jsonData = None
+#try:
+jsonData = openJson(jsonFile)
+#except ValueError as e:
+ # print "Malformed JSON?"
+
+# with open('outFile2.json','w') as out:
+#     json.dump(jsonData, out, indent=2)
+#  openJson('jsonsample.json')
+
+#print f1(jsonData)
 # def f1(values):
 
-# 	if isinstance(value,dict):
-# 		print "dict"
-# 		return f1(value.iteritems())
-# 	if isinstance(value,list):
-# 		print "list"
-# 		return f1(value[0])
-# 	else:
-# 		print "other"
-# 		return value
+
+
+
+with open('outFile.json','w') as out:
+  outData = summarizeDict(jsonData)
+  if jsonIsList:
+    outData = [outData[tempListKey]]
+  json.dump(outData, out, indent=2)
+
+
+#   if isinstance(value,dict):
+#     print "dict"
+#     return f1(value.iteritems())
+#   if isinstance(value,list):
+#     print "list"
+#     return f1(value[0])
+#   else:
+#     print "other"
+#     return value
 
 # outdata = {}
 
 # #print f1(jsonData.iteritems())
 # for key,value in jsonData.iteritems():
-# 	for key, value in f1(value.iteritems()):
-# 		print key
-# 		print value
+#   for key, value in f1(value.iteritems()):
+#     print key
+#     print value
 
 # print outdata
 
@@ -113,9 +150,8 @@ print f1(jsonData)
 #data = json.load(jsonData)
 
 # for i in  get_all(jsonData):
-# 	print(i)
+#   print(i)
 # for key in jsonData['items']['item']:
-# 	print key
+#   print key
 
 
-jsonFile.close()
